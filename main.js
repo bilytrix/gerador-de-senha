4720491f66a0b7965814e87d524b9ed8e53774a8
beatriz costa de souza <137806317+bilytrix@users.noreply.github.com>
2025-06-17 11:16:41 -0300

    ewr

--- main.js
+++ main.js
@@ -5,10 +5,23 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1;
      numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 
 console.log(botoes)
