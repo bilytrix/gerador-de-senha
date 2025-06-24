8f32faa4694042dd3584184dee38b66de543837a
beatriz costa de souza <137806317+bilytrix@users.noreply.github.com>
2025-06-24 11:04:37 -0300

    ih

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
 function geraSenha(){
-    let senha = '' ”;
+    let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
